016a0bc71ccbbedd9031b6e6bde3d0df1ce68b00
cenaice <victorphiathep@gmail.com>
2022-06-06 02:07:02 -0400

    6/6/22 12:32AM Added Player Class

--- main.py
+++ main.py
@@ -17,11 +17,6 @@ area = pygame.Rect(100, 150, 200, 124)
 testX = 750
 testY = 500
 
-
-def show_score (x, y):
-    score = font.render('Points : %s' % str(total_points), True, (255, 255, 255))
-    screen.blit(score, (testX, testY))
-
 # Loading images (venv/props/)
 gym_bg = pygame.image.load("map.png")
 punching_bag = pygame.image.load("yoga_mat.png")
@@ -33,17 +28,30 @@ tread_mill = pygame.image.load("tread_mill.png")
 icon = pygame.image.load("logo.png")
 pygame.display.set_icon(icon)
 
-
-#GLOBAL VARIABLES
+# GLOBAL VARIABLES
 total_points = 0
 timer_event = pygame.USEREVENT + 1
 pygame.time.set_timer(timer_event, 1000)  # (do this, every this milliseconds)
 clock = pygame.time.Clock()
 
+
+# Game Running
+
+class Player():
+    def __init__(self):
+        self.total_points = 0
+
+
+player = Player()
+
+
+def show_score(x, y):
+    score = font.render('Points : %s' % str(player.total_points), True, (255, 255, 255))
+    screen.blit(score, (testX, testY))
+
+
 # Game Running
 running = True
-
-
 while running:
     pygame.display.update()
     # Background Image
@@ -58,22 +66,11 @@ while running:
         clock.tick(60)
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == timer_event:
-            total_points += upgrade.upgradeMultiply()
+        # elif event.type == timer_event:
+            # player.total_points += 1
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                total_points += 1
-
-
-
-
-
-
-
+                player.total_points += 1
 
     show_score(testX, testY)
 pygame.quit()
-
-
-
-
--- upgrade.py
+++ upgrade.py
@@ -1,10 +1,35 @@
 # GLOBAL VARIABLES
-grandmas = 0
-factory = 0
+yoga_mats = 0
+punching_bag = 0
+treadmills = 0
+dead_lift = 0
+db_rack = 0
+dumbbells = 0
+squat_rack = 0
 
 
-def upgradeMultiply():
-    return (grandmas * 3) + (factory * 50)
+def yoga_upgrade():
+    pass
+
+def punching_bag():
+    pass
+
+def treadmills():
+    pass
+
+def dead_lift():
+    pass
+
+def db_rack():
+    pass
+
+def dumbbells():
+    pass
+
+def squat_rack():
+    pass
+
+
 
 
 
