4e1d2f9b94639b0ed19e9af9135892eb57e565ca
cenaice <victorphiathep@gmail.com>
2022-06-06 00:24:34 -0400

    6/6/22 12:24AM

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import pygame
-print('hi')
-background_colour = (255,255,255)
+import os
+
+background_colour = (255, 255, 255)
 (width, height) = (960, 540)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Project Esporta')
 
-screen.fill(background_colour)
-pygame.display.flip()
+# Loading images
+gym_bg = pygame.image.load("venv/props/gym_background.png")
+yoga_mat = pygame.image.load("venv/props/yoga_mat.png")
+deadlift = pygame.image.load("venv/props/deadlift.png")
+db_rack = pygame.image.load("venv/props/db_rack.png")
+bench_press = pygame.image.load("venv/props/bench_press.png")
+tread_mill = pygame.image.load("venv/props/tread_mill.png")
 
+# icon = pygame.image.load(IMAGE_NAME)
+# pygame.display.set_icon(icon)
+
+
+
+# Game Running
 running = True
 while running:
-    print('hi')
+    pygame.display.update()
+    # Background Image
+    screen.blit(gym_bg, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
 
 
 pygame.quit()
